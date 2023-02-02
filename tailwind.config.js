0228502ac191ec9b149ca3dabe146b06eb6b6076
Jakub <jakubmoz003@gmail.com>
2023-02-02 19:48:45 +0800

    add: new theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,31 +18,31 @@ module.exports = {
 				jetBrainsMono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
 			},
 			colors: {
-				theme: {
-					primary: "var(--color-primary-100)",
-					"primary-hover": "var(--color-primary-200)",
-					"primary-soft-hover": "var(--color-primary-300)",
-
-					blue: "var(--color-blue)",
-					"blue-accent": "var(--color-blue-accent)",
-
-					gray: "var(--color-gray)",
-					"gray-accent": "var(--color-gray-accent)",
-
-					green: "var(--color-green)",
-					"green-accent": "var(--color-green-accent)",
-
-					red: "var(--color-red)",
-					"red-accent": "var(--color-red-accent)",
-
-					yellow: "var(--color-yellow)",
-					"yellow-accent": "var(--color-yellow-accent)",
-
-					purple: "var(--color-purple)",
-					"purple-accent": "var(--color-purple-accent)",
-
-					white: "var(--color-white)",
-					"white-accent": "var(--color-white)",
+				primary: {
+					0: "--color-primary-100",
+					50: "--color-primary-100",
+					100: "--color-primary-100",
+					200: "--color-primary-100",
+					300: "--color-primary-100",
+					400: "--color-primary-100",
+					500: "--color-primary-100",
+					600: "--color-primary-100",
+					700: "--color-primary-100",
+					800: "--color-primary-100",
+					900: "--color-primary-100",
+				},
+				"secondary": {
+					0: "--color-primary-100",
+					50: "--color-primary-100",
+					100: "--color-primary-100",
+					200: "--color-primary-100",
+					300: "--color-primary-100",
+					400: "--color-primary-100",
+					500: "--color-primary-100",
+					600: "--color-primary-100",
+					700: "--color-primary-100",
+					800: "--color-primary-100",
+					900: "--color-primary-100",
 				},
 				template: {
 					100: "var(--color-template-100)",
@@ -54,33 +54,6 @@ module.exports = {
 					700: "var(--color-template-700)",
 					800: "var(--color-template-800)",
 					900: "var(--color-template-900)",
-				},
-			},
-			textColor: {
-				theme: {
-					base: "var(--color-text-100)",
-					accent: "var(--color-text-200)",
-					element: "var(--color-text-300)",
-					content: "var(--color-text-400) ",
-					muted: "var(--color-text-500)",
-				},
-			},
-			borderColor: {
-				theme: {
-					100: "var(--color-border-100)",
-					200: "var(--color-border-200)",
-					300: "var(--color-border-300)",
-					400: "var(--color-border-400)",
-					500: "var(--color-border-500)",
-				},
-			},
-			backgroundColor: {
-				theme: {
-					100: "var(--color-background-100)",
-					200: "var(--color-background-200)",
-					300: "var(--color-background-300)",
-					400: "var(--color-background-400)",
-					500: "var(--color-background-500)",
 				},
 			},
 		},
