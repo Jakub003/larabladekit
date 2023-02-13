3199e5120e0d9a55c5e2100a707a7e0b4633488b
Muhammad Sarmad <muhammadsarmad@codesorbit.com>
2023-02-13 19:47:32 +0500

    add google captcha validation on client side.

--- resources/views/components/google-recaptcha.blade.php
+++ resources/views/components/google-recaptcha.blade.php
@@ -10,3 +10,24 @@
 @enderror
 
 <script src='https://www.google.com/recaptcha/api.js' async defer></script>
+
+<script>
+    window.addEventListener('load', () => {
+        const $recaptcha = document.querySelector('#g-recaptcha-response');
+        if ($recaptcha) {
+            $recaptcha.setAttribute('required', 'required');
+        }
+    })
+</script>
+
+<style>
+    #g-recaptcha-response {
+        display: block !important;
+        position: absolute;
+        margin: -78px 0 0 0 !important;
+        width: 302px !important;
+        height: 76px !important;
+        z-index: -999999;
+        opacity: 0;
+    }
+</style>
